328616bd301b46e814e74e1957d794d1818afd73
Artak <artak.martirosyan@zscaler.com>
2025-12-30 16:46:13 +0400

    Moved memory leak tracker to common space, added memory leak tracking for cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUserCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUserCaseTests.swift
@@ -55,10 +55,12 @@ final class CacheFeedUserCaseTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+    private func makeSUT(file: StaticString = #file,
+                         line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
         let store = FeedStore()
         let sut = LocalFeedLoader(store: store)
-  
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
     }
 
