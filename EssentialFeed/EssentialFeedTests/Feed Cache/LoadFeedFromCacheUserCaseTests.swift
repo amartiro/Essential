5fcf10b2247cbcc41c6b19debe1fa74cbc61c2d3
Artak <artak.martirosyan@zscaler.com>
2026-01-03 19:23:30 +0400

    Replace load command completion to return result type rather than an optional error, load command delivers no emages on empty cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUserCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUserCaseTests.swift
@@ -28,14 +28,38 @@ final class LoadFeedFromCacheUserCaseTests: XCTestCase {
         let retrievalError = anyNSError()
         let exp = expectation(description: "Wait for load completion")
         var receivedError: Error?
-        sut.load { error in
-            receivedError = error
+        sut.load { result in
+            switch result {
+            case let .failure(error):
+                receivedError = error
+            default:
+                XCTFail("Expected failure, got \(result) instead")
+            }
             exp.fulfill()
         }
         
         store.completeRetrieval(with: retrievalError)
         wait(for: [exp], timeout: 1.0)
         XCTAssertEqual(receivedError as? NSError, retrievalError)
+    }
+    
+    func test_load_deliversNoImageOnEmptyCache() {
+        let (sut, store) = makeSUT()
+        let exp = expectation(description: "Wait for load completion")
+        var receivedImages: [FeedImage]?
+        sut.load { result in
+            switch result {
+            case let .success(images):
+                receivedImages = images
+            default:
+                XCTFail("Expected success, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        store.completeRetrievalWithEmptyCache()
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertEqual(receivedImages, [])
     }
     
     // MARK: - Helpers
