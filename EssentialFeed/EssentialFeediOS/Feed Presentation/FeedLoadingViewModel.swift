29b9161fc27060cb0199aba239e36024a68bee0e
Artak <artak.martirosyan@zscaler.com>
2026-02-02 18:30:01 +0400

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Artak Martirosyan on 02.02.26.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
+
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Artak Martirosyan on 02.02.26.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
