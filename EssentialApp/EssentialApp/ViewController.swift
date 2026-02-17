1434e854a5dd81446ce567de9720320683cdf587
Artak <artak.martirosyan@zscaler.com>
2026-02-17 14:32:59 +0400

    Create workspace and integrated frameworks

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Artak Martirosyan on 17.02.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
