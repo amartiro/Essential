e3829eb8e8ff323cd0525d11342c4e6079818a4b
Artak <artak.martirosyan@zscaler.com>
2026-02-23 13:26:36 +0400

    Fist UI test

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,20 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Artak Martirosyan on 19.02.26.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+    
+    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
+        let app = XCUIApplication()
+        
+        app.launch()
+        
+        XCTAssertEqual(app.cells.count, 22)
+        XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+    }
+}
