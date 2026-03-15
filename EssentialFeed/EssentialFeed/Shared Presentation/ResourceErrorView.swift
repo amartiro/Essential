f0106a262372dc439eb92933920dbbeab84fe506
Artak <artak.martirosyan@zscaler.com>
2026-03-15 11:42:33 +0400

    Rename and move ResourceErrorView and ViewModel to Shared module

---
+++ EssentialFeed/EssentialFeed/Shared Presentation/ResourceErrorView.swift
@@ -0,0 +1,10 @@
+//
+//  ResourceErrorView.swift
+//  EssentialFeed
+//
+//  Created by Artak Martirosyan on 15.03.26.
+//
+
+public protocol ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel)
+}
