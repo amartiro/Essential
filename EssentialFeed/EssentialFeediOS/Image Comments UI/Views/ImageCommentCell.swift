b25cd9fa9160eed9e91ec968b4f1f081b215986c
Artak <artak.martirosyan@zscaler.com>
2026-03-20 19:52:07 +0400

    Implement Image Comments UI

--- EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -5,3 +5,10 @@
 //  Created by Artak Martirosyan on 20.03.26.
 //
 
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
